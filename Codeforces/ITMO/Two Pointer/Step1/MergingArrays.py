ef88887b7adc5d960fa2efc8864405604bf2ed86
RishabhPichholiya <74259979+RishabhPichholiya@users.noreply.github.com>
2023-01-17 16:19:14 +0530

    Python Code for Merging Arrays

--- Codeforces/ITMO/Two Pointer/Step1/MergingArrays.py
+++ Codeforces/ITMO/Two Pointer/Step1/MergingArrays.py
@@ -1,1 +1,24 @@
 
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split())) 
+c=[] 
+i=0 
+j=0
+while(i!=n and j!=m):
+    if(a[i]<b[j]):
+        c.append(a[i]) 
+        i+=1 
+    elif(a[i]==b[j]):
+        c.append(a[i]) 
+        i+=1 
+    else:
+        c.append(b[j])
+        j+=1 
+if(i==n):
+    for it in range(j,m):
+        c.append(b[it])
+elif(j==m):
+    for it in range(i,n):
+        c.append(a[it]) 
+print(*c)
